8a1d35d900f070c654ffdea0ca9dfdcd9f4ae4aa
h586643 <54101808+h586643@users.noreply.github.com>
2020-04-20 22:17:40 +0200

    Update ProsjektDAO.java

--- Oblig3_DAT107/src/no/hvl/dat107/dao/ProsjektDAO.java
+++ Oblig3_DAT107/src/no/hvl/dat107/dao/ProsjektDAO.java
@@ -1,13 +1,16 @@
 package no.hvl.dat107.dao;
 
 
-import no.hvl.dat107.entity.*;
+import java.util.List;
 
+import no.hvl.dat107.entity.*;
 
 import javax.persistence.*;
 
 
 public class ProsjektDAO {
+	
+
 	
 	private EntityManagerFactory emf;
 	
@@ -17,6 +20,68 @@ public class ProsjektDAO {
 	}
 	
 	
-	public void leggTilNyttProsjekt(Prosjekt p) {
+	
+	public Prosjekt finnProsjektMedID(int id) {
+		
+		EntityManager em = emf.createEntityManager();
+		
+		
+		Prosjekt p1; 
+		
+		try {
+	
+	
+			p1 = em.find (Prosjekt.class, id);
+	
+		} finally {
+			
+			em.close();
+			
+		}
+		
+		return p1;
+	
+	}
+	
+	
+	public List<Prosjekt> hentAlleProsjekter(){
+		
+		EntityManager em = emf.createEntityManager();
+		List<Prosjekt> prosjekter = null; 
+		
+		try {
+			
+			TypedQuery<Prosjekt> query = em.createNamedQuery("hentAlleProsjekt", Prosjekt.class);
+			prosjekter = query.getResultList();
+			
+		}finally {
+			em.close();
+			
+		}
+		return prosjekter; 
+		
+	}
+	
+	public void opprettProsjekt(Prosjekt p) {
+		
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction tx = em.getTransaction();		
+		
+		try {
+			
+			tx.begin();
+			em.persist(p);
+			tx.commit();
+			
+		} catch (Throwable e) {
+			e.printStackTrace();
+			tx.rollback();
+			
+		}finally {
+			em.close();
+			
+		}
+	
+	
 	}
 }
